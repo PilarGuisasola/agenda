d6c1e7d590f3d6497fa25e276121602d7d7eaaa9
Pilar <pili_guisasola@icloud.com>
2025-10-16 22:22:03 -0300

    hoy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
-import { ContactsPage } from './pages/contacts-page/contacts-page';
-import { ContactDetailsPage } from './pages/contact-details-page/contact-details-page';
+import { ContactsPage } from './pages/contact-page/contact-page';
+import { ContactDetailsPage } from './pages/contact-details/contact-details';
 import { LoggedLayout } from './layouts/logged-layout/logged-layout';
 import { GroupsPage } from './pages/groups/groups';
 import { RegisterPage } from './pages/register-page/register-page';
 import { onlyPublicUserGuard } from './guards/only-public-user-guard';
-import { onlyLoggedUserGuard } from './guards/only-logged-user-guard';
+import { onlyLoggedUserGuard } from './guards/only-logged-user-guards';
 import { NewEditContact } from './pages/new-edit-contact/new-edit-contact';
 
 export const routes: Routes = [
